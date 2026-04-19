983e0e9871402853c2376d4c00e5e9f531f68779
Foster Amponsah <foster.asante@gwosevo.com>
2026-04-19 10:14:33 +0000

    Route parameter

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/users/{username}/{age}/{address}', function($a, $b, $c){
+    // return "Hello world";
+    return json_encode([
+        "username" => $a,
+        "age" => $b,
+        "address" => $c
+    ]);
 });
